9d3dfede7af98f24efa63b910fe284816fbb404e
Talfaza <charifyahia4@gmail.com>
2025-11-12 11:16:03 +0100

    delete: old stuff

--- testPipeline/testPipeline/src/main/java/com/example/testPipeline/repo/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.testPipeline.repo;
-
-
-import com.example.testPipeline.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {}
