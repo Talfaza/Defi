bcb0d410fcf2d34f5be44489968946d536730f61
kawtar-tns <tanassakawtar@gmai.com>
2025-11-12 20:55:57 +0100

    JPA+DB+PROPERTIES

--- Dockerfile
+++
@@ -1,1 +0,0 @@
-docker run --name defi_db -e POSTGRES_PASSWORD=psps -e POSTGRES_USER=user -e POSTGRES_DB=defi_db -p 5432:5432 -d postgres
---
+++ src/main/java/com/example/defi/DefiBackendApplication.java
@@ -0,0 +1,11 @@
+package com.example.defi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefiBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DefiBackendApplication.class, args);
+    }
+}
