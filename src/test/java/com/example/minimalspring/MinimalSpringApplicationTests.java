cc6b2ac3b67288e974a01868b2b851d9c452b8fa
Talfaza <charifyahia4@gmail.com>
2025-11-12 11:25:47 +0100

    pipeline test

---
+++ src/test/java/com/example/minimalspring/MinimalSpringApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.minimalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
